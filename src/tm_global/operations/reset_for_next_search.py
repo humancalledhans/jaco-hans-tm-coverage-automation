38132c8c4be69c97f7da958d69cddc511c61e45f
Gyn <Gyn>
2022-11-25 17:14:23 +0800

    fixed url issue

--- src/tm_global/operations/reset_for_next_search.py
+++ src/tm_global/operations/reset_for_next_search.py
@@ -3,8 +3,21 @@ from src.tm_global.operations.select_state import select_state
 
 
 def reset_for_next_search(driver, a):
-    if (driver.current_url == 'https://wholesalepremium.tm.com.my/coverage-search/result'):
+    if (
+        driver.current_url
+        == "https://wholesalepremium.tm.com.my/coverage-search/result"
+    ):
         driver.get("https://wholesalepremium.tm.com.my/coverage-search/address")
+        (driver, a) = pause_until_loaded(driver, a)
+        try:
+            (driver, a) = select_state(driver, a)
+            return (driver, a)
+        except Exception as e:
+            print(e)
+    elif (
+        driver.current_url == "https://wholesalepremium.tm.com.my/install/search/result"
+    ):
+        driver.get("https://wholesalepremium.tm.com.my/install/search/address")
         (driver, a) = pause_until_loaded(driver, a)
         try:
             (driver, a) = select_state(driver, a)
