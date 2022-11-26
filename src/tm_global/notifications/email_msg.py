8420d7187f15b7b24588e9e65d6030014a721c05
humancalledhans <workhansyap@gmail.com>
2022-11-26 20:00:47 +0800

    after fixing email, without html

--- src/tm_global/notifications/email_msg.py
+++ src/tm_global/notifications/email_msg.py
@@ -1,13 +1,18 @@
+from datetime import datetime
 import smtplib
 import time
 
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from src.tm_global.singleton.current_db_row import CurrentDBRow
 
 
 def setup_notification_text(text):
     current_db_row = CurrentDBRow.get_instance()
-    email_text = """
+    email_text = f"""
+Current Date Time: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
+
 Id: %s
 Source: %s
 Source Trx id: %s
@@ -20,6 +25,8 @@ City: %s
 State: %s
 Postcode: %s
     %s
+
+{datetime.now().strftime("%d/%m/%Y %H:%M:%S")} End of message.
 	""" % (current_db_row.get_id(self=current_db_row), current_db_row.get_source(self=current_db_row),
         current_db_row.get_source_id(
             self=current_db_row), current_db_row.get_house_unit_lotno(self=current_db_row),
@@ -35,20 +42,26 @@ Postcode: %s
 
 
 def send_email(text, email_to):
+    print('send email method called')
     # print("EMAIL_TO................", email_to)
     gmail_user = 'botourssp@gmail.com'
     gmail_password = 'jshmktlmwgeginnx'
     current_db_row = CurrentDBRow.get_instance()
+    content = setup_notification_text(text)
 
     if len(email_to) > 0:
         email_to_list = email_to.split(',')
 
         for email in email_to_list:
             email_to = email.strip()
+            # email_to = 'hansmaildump@gmail.com'
             # subject = 'Coverage Automation Notification'
+            print("CURRENT ID", current_db_row.get_id(self=current_db_row))
             subject = f'ACC TID {current_db_row.get_id(self=current_db_row)}'
             body = EmailMessage()
-            body.set_content = setup_notification_text(text)
+            body2 = EmailMessage()
+            body.set_content(content)
+            body.set_default_type("text/html")
 
             # print("SENDING MESSAGE...")
             try:
@@ -58,13 +71,14 @@ def send_email(text, email_to):
                 # smtp_server.close()
                 body['Subject'] = subject
                 body['From'] = gmail_user
-                body['To'] = 'hansmaildump@gmail.com'
-                with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp_server:
-                    smtp_server.login(gmail_user, gmail_password)
-                    smtp_server.send_message(body)
-                    smtp_server.quit()
+                body['To'] = email_to
+                smtp_server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+                smtp_server.login(gmail_user, gmail_password)
+                smtp_server.send_message(body)
+                smtp_server.quit()
             except Exception as ex:
-                break
+                # break
+                print(ex)
 
 
 if __name__ == '__main__':
