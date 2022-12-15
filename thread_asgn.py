322ac0fe079d9601fe28e4c0c174c53b577f846f
NitinMa2 <nitinmathew.official@gmail.com>
2022-12-15 13:22:44 +0800

    feat: encapsulating the state selection logic

--- thread_asgn.py
+++ thread_asgn.py
@@ -70,7 +70,7 @@ if __name__ == '__main__':
     num_of_iterations = 1  # jaco, change this line.
     num_of_iterations_instance = NumOfIterations.get_instance()
     num_of_iterations_instance.set_num_of_iterations(int(num_of_iterations))
-    thread_asgn = ThreadAsgn()
+    thread_asgn = ThreadAsgn(1373,1373)
     thread_asgn.start_threads()
 
     # x = threading.Thread(target=func)
