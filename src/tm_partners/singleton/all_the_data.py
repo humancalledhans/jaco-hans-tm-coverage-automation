55294b4c51a15f5c6f5a044a533e6e9909570260
gyn <bitbucket@tracking.my>
2022-12-17 19:05:45 +0800

    changed singletons in tm partner

--- src/tm_partners/singleton/all_the_data.py
+++ src/tm_partners/singleton/all_the_data.py
@@ -1,42 +1,40 @@
 from abc import ABCMeta, abstractstaticmethod
+import threading
 
 
 class IAllTheData(metaclass=ABCMeta):
-
     @abstractstaticmethod
     def add_into_data_list():
-        """ to implement in child class """
+        """to implement in child class"""
 
     @abstractstaticmethod
     def set_data_list():
-        """ to implement in child class """
+        """to implement in child class"""
 
     @abstractstaticmethod
     def get_all_the_data_list():
-        """ to implement in child class """
+        """to implement in child class"""
 
     @staticmethod
     def reset_all_data():
-        """ to implement in child class """
+        """to implement in child class"""
 
 
 class AllTheData(IAllTheData):
-
-    __instance = None
-
     @staticmethod
     def get_instance():
-        if AllTheData.__instance is None:
-            AllTheData()
-        return AllTheData.__instance
+        local = threading.current_thread().__dict__
+        try:
+            instance = local["all_the_data_instance"]
+        except KeyError:
+            local["all_the_data_instance"] = AllTheData()
+            instance = local["all_the_data_instance"]
+        if instance is None:
+            instance = AllTheData()
+        return instance
 
     def __init__(self):
-        if AllTheData.__instance is not None:
-            raise Exception(
-                "AllTheData instance cannot be instantiated more than once!")
-        else:
-            self.all_the_data_list = []
-            AllTheData.__instance = self
+        self.all_the_data_list = []
 
     @staticmethod
     def add_into_data_list(self, data):
