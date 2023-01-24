941163b042c94549a1451ded5e8edf73c829aa65
NitinMa2 <nitinmathew.official@gmail.com>
2023-01-24 13:47:39 +0800

    fix: removing test code

--- src/tm_partners/coverage_check/coverage_check.py
+++ src/tm_partners/coverage_check/coverage_check.py
@@ -50,7 +50,6 @@ class FindingCoverage:
         pass
 
     def finding_coverage(self, driver, a):
-        raise Exception("break up")
 
         (driver, a) = input_speed_requested(driver, a, 50)
 
